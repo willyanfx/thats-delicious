aa292fc7eccbfba6531af9fc13a1f6455406dcfc
willyanfx <willyan.fx@gmail.com>
2024-12-26 11:00:38 -0700

    drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,10 +2,9 @@ import { defineConfig } from "drizzle-kit";
 if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL is not set");
 
 export default defineConfig({
-  schema: "./src/db/schema",
-
+  schema: "./src/database/schema.ts",
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL!,
   },
   strict: true,
   dialect: "sqlite",
